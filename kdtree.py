255af8d8abaac7d457885897b0f22314f0ac977c
batel.ahmed@icloud.com <batel.ahmed@icloud.com>
2026-07-25 11:26:06 -0700

    add closer_point

--- kdtree.py
+++ kdtree.py
@@ -1,2 +1,12 @@
 def distance_squared(a, b):
     return sum((x - y) ** 2 for x, y in zip(a, b))
+
+
+def closer_point(target, p1, p2):
+    if p1 is None:
+        return p2
+    if p2 is None:
+        return p1
+    if distance_squared(target, p1) < distance_squared(target, p2):
+        return p1
+    return p2
