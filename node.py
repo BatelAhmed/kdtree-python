8cfaa83a4239bb2385f63e160777d5a71ac3e96b
batel.ahmed@icloud.com <batel.ahmed@icloud.com>
2026-07-26 13:14:38 -0700

    update node.py

--- node.py
+++ node.py
@@ -1,4 +1,7 @@
 class Node:
+    # nodes only ever hold these three, saves memory on big trees
+    __slots__ = ("point", "left", "right")
+
     def __init__(self, point):
         self.point = point
         self.left = None
